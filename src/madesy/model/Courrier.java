b4963899353d87d4bbd0c1103d50ecfd487fc590
Hristo Danovski <hr.danovski@gmail.com>
2013-10-03 16:57:19 +0300

    Added Courrier and PickingStorage dispatch method

---
+++ src/madesy/model/Courrier.java
@@ -0,0 +1,38 @@
+package madesy.model;
+
+import java.util.Random;
+
+import madesy.storage.PickingStorage;
+
+public class Courrier extends Person {
+	
+	public Courrier(int id, String name, PickingStorage pickingStorage)
+	{
+		super(id, name, pickingStorage);
+	}
+	
+	public void run() {
+		dispatchPicking();
+	}
+	
+	private void dispatchPicking() {
+		Picking dispatchedPicking = pickingStorage.pickingToDispatch();
+		
+		if(dispatchedPicking == null)
+			return;
+		
+		Random rand = new Random();
+		
+		try {
+			Thread.sleep(rand.nextInt(5000));
+		} catch(InterruptedException e) {
+			// TODO: on interrupt?
+		}
+		
+		takePicking(dispatchedPicking);
+	}
+	
+	private void takePicking(Picking picking) {
+		pickingStorage.markPickingTaken(picking);
+	}
+}
--- src/madesy/storage/PickingStorage.java
+++ src/madesy/storage/PickingStorage.java
@@ -2,8 +2,11 @@ package madesy.storage;
 
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import madesy.model.Picking;
+import madesy.model.PickingStates;
 
 public class PickingStorage {
 	private List<Picking> pickings;
@@ -17,10 +20,25 @@ public class PickingStorage {
 	}
 	
 	public Picking pickingToDispatch() {
-		return null;
+		Picking picking = null;
+		Lock lock = new ReentrantLock();
+		lock.lock();
+		try {
+			for(int i = 0; i < pickings.size(); i ++) {
+				// get index of first picking marked as new
+				if(pickings.get(i).getPickingStates().toString().equals(PickingStates.NEW)) {
+					pickings.get(i).setPickingStates(PickingStates.DISPATCHED);
+					picking = pickings.get(i);
+					break;
+				}
+			}
+		} finally {
+			lock.unlock();
+		}
+		return picking;
 	}
 	
-	public void markPickingTaken() {
+	public void markPickingTaken(Picking picking) {
 		
 	}
 }
