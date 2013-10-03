0b0d3a36e6b1012fdeae9243ebe0588c283a4afe
gallyto <galina_hristova_@hotmail.com>
2013-10-03 16:21:08 +0300

    concurrent array list

--- src/madesy/storage/PickingStorage.java
+++ src/madesy/storage/PickingStorage.java
@@ -1,11 +1,16 @@
 package madesy.storage;
 
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import madesy.model.Picking;
 
 public class PickingStorage {
 	private List<Picking> pickings;
+	
+	public PickingStorage() {
+		pickings = new CopyOnWriteArrayList<Picking>();
+	}
 	
 	public void newPicking(Picking picking) {
 		
